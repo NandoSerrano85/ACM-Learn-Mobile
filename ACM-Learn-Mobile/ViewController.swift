adc713df4cf1d821535b9e4b02d214cefb4709e7
Fernando Serrano <fserr009@fiu.edu>
2019-11-19 01:49:04 -0500

    Initial Commit

---
+++ ACM-Learn-Mobile/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ACM-Learn-Mobile
+//
+//  Created by Fernando Serrano on 11/19/19.
+//  Copyright © 2019 FIU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ ACM-Learn-MobileTests/ACM_Learn_MobileTests.swift
@@ -0,0 +1,34 @@
+//
+//  ACM_Learn_MobileTests.swift
+//  ACM-Learn-MobileTests
+//
+//  Created by Fernando Serrano on 11/19/19.
+//  Copyright © 2019 FIU. All rights reserved.
+//
+
+import XCTest
+@testable import ACM_Learn_Mobile
+
+class ACM_Learn_MobileTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
